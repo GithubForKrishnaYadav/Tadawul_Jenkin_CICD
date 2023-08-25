90b61c73c633a8d2a9ee6e9032afa0a2dfe3ddc7
Krishna <beakrish@gmail.com>
2023-08-25 16:35:49 +0300

    removed codes

--- src/main/java/com/tadawul/tadawuljenkinscicd/TadawulJenkinsCicdApplication.java
+++ src/main/java/com/tadawul/tadawuljenkinscicd/TadawulJenkinsCicdApplication.java
@@ -3,8 +3,6 @@ package com.tadawul.tadawuljenkinscicd;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import javax.annotation.PostConstruct;
-
 @SpringBootApplication
 public class TadawulJenkinsCicdApplication {
 	public static void main(String[] args) {
